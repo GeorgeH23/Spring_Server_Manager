e5680e497d3cc9b0487927c7c4f14b8814a8e355
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-05-17 01:29:09 +0100

    Initial commit.

---
+++ src/main/java/com/george/springservermanager/SpringServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.george.springservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/george/springservermanager/SpringServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.springservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
