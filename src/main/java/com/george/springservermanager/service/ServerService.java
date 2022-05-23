3bd1b9a485a0894542adcee670d521ec5fc19dfc
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-05-23 22:09:50 +0100

    Implemented ServerService methods.

--- src/main/java/com/george/springservermanager/service/ServerService.java
+++ src/main/java/com/george/springservermanager/service/ServerService.java
@@ -2,12 +2,13 @@ package com.george.springservermanager.service;
 
 import com.george.springservermanager.domain.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
 
     Server create(Server server);
-    Server pingServer(String ipAddress);
+    Server pingServer(String ipAddress) throws IOException;
     Collection<Server> serverList(int limit);
     Server getServer(Long id);
     Server updateServer(Server server);
--- src/main/java/com/george/springservermanager/service/ServerServiceImpl.java
+++ src/main/java/com/george/springservermanager/service/ServerServiceImpl.java
@@ -1,39 +1,72 @@
 package com.george.springservermanager.service;
 
 import com.george.springservermanager.domain.Server;
+import com.george.springservermanager.repository.ServerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 
+import static com.george.springservermanager.enumeration.Status.SERVER_DOWN;
+import static com.george.springservermanager.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.*;
+import static org.springframework.data.domain.PageRequest.*;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
 
+    private final ServerRepository serverRepository;
 
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
-    public Server pingServer(String ipAddress) {
-        return null;
+    public Server pingServer(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> serverList(int limit) {
-        return null;
+        log.info("Fetching all servers...");
+        return serverRepository.findAll(of(0, limit)).toList();
     }
 
     @Override
     public Server getServer(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepository.findById(id).stream().findFirst().get();
     }
 
     @Override
     public Server updateServer(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepository.save(server);
     }
 
     @Override
     public Boolean deleteServer(Long id) {
+        log.info("Deleting server by ID: {}", id);
+        serverRepository.deleteById(id);
+        return TRUE;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
