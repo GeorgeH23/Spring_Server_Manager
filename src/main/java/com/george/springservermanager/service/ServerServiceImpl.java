40d6a3fd4071c27cc41781d60be51c9f0158da0c
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-05-23 22:29:22 +0100

    Implemented .setServerUrl() method.

--- src/main/java/com/george/springservermanager/service/ServerServiceImpl.java
+++ src/main/java/com/george/springservermanager/service/ServerServiceImpl.java
@@ -5,11 +5,13 @@ import com.george.springservermanager.repository.ServerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static com.george.springservermanager.enumeration.Status.SERVER_DOWN;
 import static com.george.springservermanager.enumeration.Status.SERVER_UP;
@@ -67,6 +69,9 @@ public class ServerServiceImpl implements ServerService {
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
