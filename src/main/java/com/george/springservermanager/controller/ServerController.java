e26949e873b0d1b53563b8019075bf59c424baeb
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-05-24 22:09:25 +0100

    Implemented pingServer() endpoint.

--- src/main/java/com/george/springservermanager/controller/ServerController.java
+++ src/main/java/com/george/springservermanager/controller/ServerController.java
@@ -1,14 +1,19 @@
 package com.george.springservermanager.controller;
 
 import com.george.springservermanager.domain.Response;
+import com.george.springservermanager.domain.Server;
 import com.george.springservermanager.service.ServerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 import java.util.Map;
 
+import static com.george.springservermanager.enumeration.Status.*;
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.*;
 
@@ -26,6 +31,20 @@ public class ServerController {
                         .timeStamp(now())
                         .data(Map.of("servers", serverService.serverList(30)))
                         .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.pingServer(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping success" : "Ping failed")
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
