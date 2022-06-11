e4ab093b74bcd8d7c473431ca0a1f437203854e1
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-06-11 22:44:05 +0100

    Added condition to check for existing database entries.

--- src/main/java/com/george/springservermanager/SpringServerManagerApplication.java
+++ src/main/java/com/george/springservermanager/SpringServerManagerApplication.java
@@ -25,10 +25,12 @@ public class SpringServerManagerApplication {
     @Bean
     CommandLineRunner run(ServerRepository serverRepository) {
         return args -> {
-            serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/Server1.png", SERVER_UP));
-            serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/Server2.png", SERVER_DOWN));
-            serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/Server3.png", SERVER_UP));
-            serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/Server4.png", SERVER_DOWN));
+            if (serverRepository.count() == 0) {
+                serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/Server1.png", SERVER_UP));
+                serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/image/Server2.png", SERVER_DOWN));
+                serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/Server3.png", SERVER_UP));
+                serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/Server4.png", SERVER_DOWN));
+            }
         };
     }
 
